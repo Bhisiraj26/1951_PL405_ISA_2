4bbf4837a416d0548f056d884c4e9fa0c1e1fe6c
Bhisiraj26 <mca.1951@unigoa.ac.in>
2021-04-29 15:18:13 +0530

    Second commit

---
+++ src/operations/Main.java
@@ -0,0 +1,25 @@
+package operations;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter the two numbers to perform operations.");
+		
+	    int n1 = 51;
+	    int n2 = 9;
+	    
+	    System.out.println(n1 + " " + n2);
+
+	    int ans_sum = n1 + n2;
+	    System.out.println("Ans of sum is:" + ans_sum);
+	    
+	    int ans_difference = n1 - n2;
+	    System.out.println("Ans of Subtraction is:" + ans_difference);
+	    
+	    int ans_multiplication = n1 * n2;
+	    System.out.println("After Multiplication answer is:" + ans_multiplication);
+	    
+	}
+
+}
